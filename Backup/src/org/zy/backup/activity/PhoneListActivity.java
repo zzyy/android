bb99934b9aac1cf8f58925622f7c402b18f5d5b2
Simon <zzyy.alex@gmail.com>
2014-05-29 07:51:20 +0800

    modify the PhoneListActivity.java

--- Backup/src/org/zy/backup/activity/PhoneListActivity.java
+++ Backup/src/org/zy/backup/activity/PhoneListActivity.java
@@ -7,11 +7,15 @@ import org.zy.backup.model.Contact;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class PhoneListActivity extends Activity {
 
@@ -26,6 +30,16 @@ public class PhoneListActivity extends Activity {
 		ArrayAdapter<Contact> adapter = new PhoneListAdapt(this);
 		adapter.addAll(Backup.newInstance().contacts);
 		phoneList.setAdapter(adapter);
+		
+		phoneList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Contact contact =  (Contact) parent.getItemAtPosition(position);
+				Toast.makeText(PhoneListActivity.this, contact.name, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 	
