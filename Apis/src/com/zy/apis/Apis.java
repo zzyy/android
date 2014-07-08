737d0942cb7768f4fbe7cdd74da1399fff81dc65
Simon <zzyy.alex@gmail.com>
2014-07-08 23:43:38 +0800

    modify apis menu

--- Apis/src/com/zy/apis/Apis.java
+++ Apis/src/com/zy/apis/Apis.java
@@ -2,6 +2,7 @@ package com.zy.apis;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -9,11 +10,8 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -23,50 +21,68 @@ public class Apis extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
-		String path = intent.getStringExtra("");
-		if(path ==null){
+		String path = intent.getStringExtra("codePath");
+		if (path == null) {
 			path = "";
 		}
-		
-		setListAdapter(new SimpleAdapter(this, getData(path), 
-				R.layout.simple_list_item, new String[]{"itemName"}, new int[]{R.id.itemText}));
+
+		setListAdapter(new SimpleAdapter(this, getData(path),
+				R.layout.simple_list_item, new String[] { "itemName" },
+				new int[] { R.id.itemText }));
 	}
 
-	private List<Map<String, Object>> getData(String path) {
-		List<Map<String, Object>> result = new ArrayList<Map<String,Object>>();
-		
+	private List<Map<String, Object>> getData(String prefix) {
+		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+
 		Intent intent = new Intent("com.zy.apis.main");
 		intent.addCategory("com.zy.apis.default_category");
-		
+
 		PackageManager pm = getPackageManager();
 		List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
-		if(null == resolveInfos)
+		if (null == resolveInfos)
 			return result;
-		for(ResolveInfo resolveInfo :resolveInfos){
+		for (ResolveInfo resolveInfo : resolveInfos) {
 			CharSequence lableSeq = resolveInfo.loadLabel(pm);
-			String[] names = resolveInfo.activityInfo.name.split(".");
-			String itemName = lableSeq != null ? lableSeq.toString() : names[names.length-1];
-			
-			Map<String , Object> item = new HashMap<String, Object>();
-			item.put("itemName" , itemName);
+			String lable = lableSeq.toString();
+
+			if ("".equals(prefix)) {
+
+			}
+
+			String itemName = null;
 			Intent itemIntent = new Intent();
-			
-			int index = resolveInfo.activityInfo.name.lastIndexOf(".");
-			itemIntent.setClassName(resolveInfo.activityInfo.applicationInfo.packageName, 
-					resolveInfo.activityInfo.name);
-			
-//			itemIntent.setClassName(resolveInfo.activityInfo.applicationInfo.packageName + resolveInfo.activityInfo.name.substring(0, index), 
-//					resolveInfo.activityInfo.name.substring(index +1));
+
+			Map<String, Boolean> entries = new HashMap<String, Boolean>();
+
+			String[] prePath = prefix.split("/");
+			String[] path = lable.split("/");
+			if (("".equals(prefix) ? 0 : prePath.length) == path.length - 1) {
+				itemName = path[path.length - 1];
+				itemIntent.setClassName(
+						resolveInfo.activityInfo.applicationInfo.packageName,
+						resolveInfo.activityInfo.name);
+			} else {
+				itemName = "".equals(prefix) ? path[0] : path[prePath.length];
+				if (entries.get(itemName) == null) {
+					itemIntent.putExtra("codePath", prefix + itemName + "/");
+					itemIntent.setClass(this, Apis.class);
+					entries.put(itemName, true);
+				}
+			}
+
+			Map<String, Object> item = new HashMap<String, Object>();
+			item.put("itemName", itemName);
 			item.put("intent", itemIntent);
 			result.add(item);
 		}
-		
+
 		return result;
 	}
-	
+
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Map<String, Object> data = (Map<String, Object>) l.getItemAtPosition(position);
+		Map<String, Object> data = (Map<String, Object>) l
+				.getItemAtPosition(position);
 		Intent intent = (Intent) data.get("intent");
 		startActivity(intent);
 	}
