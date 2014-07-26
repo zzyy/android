8c89e181d14df8743fefe280be0a220d8af6a835
zouyun <zou.yun@eastaeon.com>
2014-07-26 12:46:53 +0800

    modify animatorTest

--- Apis/src/com/zy/apis/animation/AnimatorTest.java
+++ Apis/src/com/zy/apis/animation/AnimatorTest.java
@@ -2,22 +2,33 @@ package com.zy.apis.animation;
 
 import com.zy.apis.R;
 
+import android.R.string;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.Keyframe;
 import android.animation.LayoutTransition;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class AnimatorTest extends Activity{
-
+	private static final String TAG ="AnimatorTest";
+	
+	Button b_flip;
+	Button b_scale;
+	ImageView i_image;
 	LinearLayout buttonContainer;
 	LayoutTransition mLayoutTransition;
 	@Override
@@ -50,12 +61,57 @@ public class AnimatorTest extends Activity{
 			}
 		});
 		
-		mLinearLayout.addView(buttonContainer);
+		mLinearLayout.addView(buttonContainer, 1);
 		setupAnimator();
-		
 		buttonContainer.setLayoutTransition(mLayoutTransition);
+		
+		b_flip = (Button) findViewById(R.id.flip);
+		i_image = (ImageView) findViewById(R.id.image);
+		setFlipAnimate();
+		
+		b_scale = (Button) findViewById(R.id.scale);
+		setScaleAnimate();
 	}
 	
+	private void setScaleAnimate() {
+		PropertyValuesHolder pvhWidth = PropertyValuesHolder.ofFloat("scaleX", 2f);
+		PropertyValuesHolder pvhHeight = PropertyValuesHolder.ofFloat("scaleY", 2f);
+		PropertyValuesHolder pvhPivotX = PropertyValuesHolder.ofFloat("pivotY", 0f);
+		final ObjectAnimator scaleAnimator = ObjectAnimator.ofPropertyValuesHolder(i_image, pvhPivotX, pvhHeight, pvhWidth);
+		
+		b_scale.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				scaleAnimator.start();
+			}
+		});
+	}
+
+	private void setFlipAnimate() {
+		TimeInterpolator disappearInterpolator = new AccelerateInterpolator();
+		final ObjectAnimator dissappearAnimator = ObjectAnimator.ofFloat(i_image, "rotationY", 0f, 90f);
+		dissappearAnimator.setInterpolator(disappearInterpolator);
+
+		TimeInterpolator appearInterpolator = new DecelerateInterpolator();
+		final ObjectAnimator appearAnimator = ObjectAnimator.ofFloat(i_image, "rotationY", -90f, 0f);
+		appearAnimator.setInterpolator(appearInterpolator);
+		
+		dissappearAnimator.addListener(new AnimatorListenerAdapter() {
+			@Override
+			public void onAnimationEnd(Animator animation) {
+				super.onAnimationEnd(animation);
+				appearAnimator.start();
+			}
+		});
+		
+		b_flip.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dissappearAnimator.start();
+			}
+		});
+	}
+
 	private void setupAnimator() {
 		mLayoutTransition = new LayoutTransition();
 		//change_appearing
@@ -72,10 +128,10 @@ public class AnimatorTest extends Activity{
 //		ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(buttonContainer, pvhBottom,pvhLeft,pvhRight, pvhTop);
 //		objectAnimator.setDuration(1*1000);
 		
-		PropertyValuesHolder pvhScaleX = PropertyValuesHolder.ofFloat("ScaleX", 0f, 2f, 0f ,1f);
-		PropertyValuesHolder pvhScaleY = PropertyValuesHolder.ofFloat("ScaleY", 0f, 2f, 0f ,1f);
-		PropertyValuesHolder pvhScaleXInv = PropertyValuesHolder.ofFloat("ScaleX", 0f, 2f, 0f );
-		PropertyValuesHolder pvhScaleYInv = PropertyValuesHolder.ofFloat("ScaleY", 0f, 2f, 0f );
+		PropertyValuesHolder pvhScaleX = PropertyValuesHolder.ofFloat("ScaleX", 0f, 1f, 0f ,1f);
+		PropertyValuesHolder pvhScaleY = PropertyValuesHolder.ofFloat("ScaleY", 0f, 1f, 0f ,1f);
+		PropertyValuesHolder pvhScaleXInv = PropertyValuesHolder.ofFloat("ScaleX", 0f, 1f, 0f );
+		PropertyValuesHolder pvhScaleYInv = PropertyValuesHolder.ofFloat("ScaleY", 0f, 1f, 0f );
 		ObjectAnimator apperaing =  ObjectAnimator.ofPropertyValuesHolder(buttonContainer, pvhScaleX, pvhScaleY, pvhRotation);
 		ObjectAnimator changeAppearing = ObjectAnimator.ofPropertyValuesHolder(buttonContainer, pvhLeft, pvhTop, pvhRotation, pvhScaleX, pvhScaleY);
 		ObjectAnimator disappearing = ObjectAnimator.ofPropertyValuesHolder(buttonContainer,pvhScaleXInv,pvhScaleYInv);
